77ef03699e54a2f015a78072992da40a6ab99236
samsesz <samiamunizmuniz@gmail.com>
2025-09-10 22:30:37 -0300

    aula5

--- 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
+++ 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
@@ -5,9 +5,25 @@ import userController from "../controllers/userController.js";
 const Authorization = (req, res, next) => {
   const authToken = req.headers["authorization"];
   if (authToken != undefined) {
-    next();
+    //Dividindo a string do token (eliminar palavra bearer)
+    const bearer = authToken.split(" ");
+    const token = bearer[1];
+    //Validando o token
+    jwt.verify(token, userController.jwtSecret, (error, data) => {
+      if (error) {
+        res.status(401).json({ error: "token invalido" });
+        // token valido
+      } else {
+        req.token = token;
+        req.loggedUser = {
+          id: data.id,
+          email: data.email,
+        }
+        next();
+      }
+    });
   } else {
-    res.status(401).json({ error: "token invalido" });
+    res.status(401).json({ error: "Acesso não autorizado" });
   }
 };
 
